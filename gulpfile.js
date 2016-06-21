827e30579d982360d266d812ff8e85de50a3901f
Simon Linder <simon.h.linder@gmail.com>
2016-06-21 17:46:41 -0400

    Minified our HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -17,7 +18,8 @@ var env,
     outputDir,
     sassStyle;
 
-env = process.env.NODE_ENV || 'development';
+env = 'production';
+//env = process.env.NODE_ENV || 'development';
 
 if (env==='development') {
     outputDir = 'builds/development/';
@@ -70,7 +72,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('builds/components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
@@ -82,7 +84,9 @@ gulp.task('connect',function() {
 });
 
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 });
 
